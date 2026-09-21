3cb3a812518d088eb3f2bf277def382063546efc
Daniil Penkov <dapenkoff@yandex-team.ru>
2026-09-21 13:47:26 +0300

    fix: main + weak_ptr handling

--- CDLib/CDLib.cpp
+++ CDLib/CDLib.cpp
@@ -84,11 +84,11 @@ void clear (window_handler_t windowHandler)
 {
     g_Ctx.commandManager.addCommand([=] (DrawState& state)
     {
-        if (windowHandler.expired())
+        auto windowCtx = windowHandler.lock();
+
+        if (!windowCtx)
             return;
 
-        auto windowCtx = windowHandler.lock();
-        
         windowCtx->texture.clear(state.fillColor);
     });
 }
@@ -97,10 +97,10 @@ void drawLine (int x0, int y0, int x1, int y1, window_handler_t windowHandler)
 {
     g_Ctx.commandManager.addCommand([=] (DrawState& state) mutable
     {
-        if (windowHandler.expired())
-            return;
-
         auto windowCtx = windowHandler.lock();
+
+        if (!windowCtx)
+            return;
 
         if (x1 < x0)
         {
@@ -126,10 +126,10 @@ void drawCircle (int centerX, int centerY, float radius, window_handler_t window
 {
     g_Ctx.commandManager.addCommand([=] (DrawState& state)
     {
-        if (windowHandler.expired())
-            return;
-
         auto windowCtx = windowHandler.lock();
+
+        if (!windowCtx)
+            return;
 
         sf::CircleShape circle(radius);
         circle.setFillColor(state.fillColor);
@@ -147,10 +147,10 @@ void drawEllipse (int x0, int y0, int x1, int y1, window_handler_t windowHandler
 {
     g_Ctx.commandManager.addCommand([=] (DrawState& state) mutable
     {
-        if (windowHandler.expired())
-            return;
-
         auto windowCtx = windowHandler.lock();
+
+        if (!windowCtx)
+            return;
 
         if (x1 < x0)
             std::swap(x0, x1);
@@ -184,10 +184,10 @@ void drawRect (int x0, int y0, int x1, int y1, window_handler_t windowHandler)
 {
     g_Ctx.commandManager.addCommand([=] (DrawState& state) mutable
     {
-        if (windowHandler.expired())
-            return;
-
         auto windowCtx = windowHandler.lock();
+
+        if (!windowCtx)
+            return;
 
         if (x1 < x0)
             std::swap(x0, x1);
@@ -216,10 +216,10 @@ void drawPolygon (sf::Vector2f points[], uint32_t numPoints, window_handler_t wi
 
     g_Ctx.commandManager.addCommand([=] (DrawState& state) mutable
     {
-        if (windowHandler.expired())
-            return;
-
         auto windowCtx = windowHandler.lock();
+
+        if (!windowCtx)
+            return;
 
         polygon.setFillColor(state.fillColor);
         polygon.setOutlineColor(state.color);
@@ -249,10 +249,10 @@ sf::Color getPixel (int x, int y, window_handler_t windowHandler)
 
     g_Ctx.commandManager.addCommand([=] (DrawState&) mutable
     {
-        if (windowHandler.expired())
-            return;
-
         auto windowCtx = windowHandler.lock();
+
+        if (!windowCtx)
+            return;
 
         sf::Color pixel = windowCtx->texture.getTexture().copyToImage().getPixel(sf::Vector2u(x, y));
         promise->set_value(pixel);
---
+++ CDLib/WindowHandler.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+#include <limits>
+
+enum class WindowHandler : uint64_t
+{
+    Invalid = std::numeric_limits<uint64_t>::max()
+};
